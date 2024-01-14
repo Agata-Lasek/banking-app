baf4a9d1114f0c2733efc8eb5f3794bc9e72d241
raczu <Raczuekk@gmail.com>
2024-01-14 23:17:01 +0100

    fix: client now always try to get token

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: "http://localhost:3004/api/v1"
+    baseURL: "http://localhost:3004/api/v1",
+    withCredentials: false,
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${localStorage.getItem('token')}`
+    }
 })
 
 export default client
